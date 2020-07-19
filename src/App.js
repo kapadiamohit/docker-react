767563da2da589d3be1fe4548aa730ed3ea4e56e
Mohit Kapadia <mohit.kapadia@hotmail.com>
2020-07-19 15:02:49 +1200

    made feature change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there! This is my first deployment from VS code to GitHub to Travis CI and then to AWS. Woohooo!!!
+          Hi there! I was changed in the feature branch.
         </p>
         <a
           className="App-link"
